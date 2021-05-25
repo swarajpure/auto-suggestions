eeea914333883e41d7321c0e76efa469a9555feb
Swaraj <swarajrajpure@gmail.com>
2021-05-26 01:35:51 +0530

    changes in abortcontroller, now doesn't abort every subsequent network call after first aborted request

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,20 +1,22 @@
 import { BOOKS_API_URL } from '../constants/constants';
 
-const abortController = new AbortController();
-let signal = abortController.signal;
+let abortController = new AbortController();
+
+const abortRequest = () => {
+  console.log('aborting....')
+  abortController.abort();
+  abortController = new AbortController();
+}
 
 const getData = async (query) => {
   try {
+    const signal = abortController.signal;
     const books = await fetch(`${BOOKS_API_URL}/${query}`, { signal });
     const booksJson = await books.json();
     return booksJson;
   } catch (err) {
     console.log(err);
   }
-}
-
-const abortRequest = () => {
-  abortController.abort();
 }
 
 export { getData, abortRequest };
